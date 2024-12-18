210f7d5174ea6f3137fb775ec0ef11fd4eac317d
nariwaa <nariwaa>
2024-12-18 15:20:17 +0100

    made progress

--- bot.py
+++ bot.py
@@ -1,11 +1,24 @@
 from nextcord.ext import commands
+import nextcord
 
-# the prefix is not used in this example
-bot = commands.Bot(command_prefix='$')
+TESTING_GUILD_ID = 1318201343382388837
 
+bot = commands.Bot()
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+# when message recived
 @bot.event
 async def on_message(message):
     print(f'Message from {message.author}: {message.content}')
+    if message.content != "":
+        print("not empty")
+        # u should call the function
+        await message.reply("dsofhiewhf")
+    else:
+        print("message empty, doing nothing")
 
 with open("secret.env", 'r') as env_file:
     env_data = env_file.readlines()
--- main.py
+++ main.py
@@ -1,11 +1,40 @@
 from openai import OpenAI
+from nextcord.ext import commands
 
-# Initialize the client to point to LM Studio
+# Initialize LM Studio
 client = OpenAI(
     base_url="http://localhost:1234/v1",
     api_key="lm-studio"  # LM Studio doesn't require an actual API key
 )
 
+# Initialize discord bot
+bot = commands.Bot()
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.slash_command(description="to know what model is being used", guild_ids=[TESTING_GUILD_ID])
+async def model(interaction: nextcord.Interaction):
+    await interaction.send("idk")
+
+# when message recived
+@bot.event
+async def on_message(message):
+    print(f'Message from {message.author}: {message.content}')
+    if message.content != "":
+        print("not empty")
+        # u should call the function
+        await message.reply("dsofhiewhf")
+    else:
+        print("message empty, doing nothing")
+
+# discord token stuff
+with open("secret.env", 'r') as env_file:
+    env_data = env_file.readlines()
+    KEY_DISCORD = env_data[0].strip()
+
+# lm-studio function
 def chat_with_lm_studio():
     model_identifier = "your-model-identifier"     
 
@@ -34,5 +63,6 @@ def chat_with_lm_studio():
         except Exception as e:
             print(f"An error occurred: {e}")
 
-if __name__ == "__main__":
-    chat_with_lm_studio()
+# # thign that do stuff
+# if __name__ == "__main__":
+#     chat_with_lm_studio()
